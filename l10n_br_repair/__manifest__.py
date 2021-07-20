c93c531c167da6fcc493c3adf17a6b06bdecd00c
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-07-20 16:37:43 -0300

    [PR OCA/1548][IMP] l10n_br_repair: repair_templates_repair_order.xml localization

--- l10n_br_repair/__manifest__.py
+++ l10n_br_repair/__manifest__.py
@@ -27,6 +27,7 @@
         'views/repair_order.xml',
         'views/repair_fee.xml',
         'views/repair_line.xml',
+        'report/repair_templates_repair_order.xml',
     ],
     'demo': [
         'demo/res_company.xml',
