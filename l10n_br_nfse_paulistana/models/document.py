4fb55adac6e9fc1b831f4cc4843556f6b6d65ced
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-05-26 20:13:47 -0300

    [FIX] l10n_br_nfse_paulistana: municipal registration is EMPTY if city different company

--- l10n_br_nfse_paulistana/models/document.py
+++ l10n_br_nfse_paulistana/models/document.py
@@ -172,8 +172,13 @@ class Document(models.Model):
                 tpCPFCNPJ(CNPJ=dados_tomador["cnpj"], CPF=dados_tomador["cpf"]),
             ),
             InscricaoMunicipalTomador=self.convert_type_nfselib(
-                tpRPS, "InscricaoMunicipalTomador", dados_tomador["inscricao_municipal"]
-            ),
+                tpRPS,
+                "InscricaoMunicipalTomador",
+                dados_tomador["inscricao_municipal"],
+            )
+            if dados_tomador["codigo_municipio"]
+            == int("%s" % (self.company_id.partner_id.city_id.ibge_code))
+            else None,
             InscricaoEstadualTomador=self.convert_type_nfselib(
                 tpRPS, "InscricaoEstadualTomador", dados_tomador["inscricao_estadual"]
             ),
@@ -215,7 +220,9 @@ class Document(models.Model):
                 ),
             ),
             ValorCargaTributaria=self.convert_type_nfselib(
-                tpRPS, "ValorCargaTributaria", dados_lote_rps["carga_tributaria_estimada"]
+                tpRPS,
+                "ValorCargaTributaria",
+                dados_lote_rps["carga_tributaria_estimada"],
             ),
             FonteCargaTributaria=self.convert_type_nfselib(
                 tpRPS, "FonteCargaTributaria", "IBPT"
@@ -273,7 +280,7 @@ class Document(models.Model):
         ).strftime("%Y%m%d")
         assinatura += self._map_taxation_rps(dados_lote_rps["natureza_operacao"])
         assinatura += "N"  # Corrigir - Verificar status do RPS
-        assinatura += "S" if dados_servico["iss_retido"] == '1' else "N"
+        assinatura += "S" if dados_servico["iss_retido"] == "1" else "N"
         assinatura += (
             ("%.2f" % dados_lote_rps["total_recebido"]).replace(".", "").zfill(15)
         )
