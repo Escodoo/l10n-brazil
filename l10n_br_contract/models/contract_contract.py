6f5443a6c20b4182cf758ab4819c9d0566e0610a
Marcel Savegnago <marcel.savegnago@gmail.com>
2022-07-27 10:17:47 -0300

    [IMP] l10n_br_contract: add parameter in company and contract to force tax recalculation before invoicing

--- l10n_br_contract/models/contract_contract.py
+++ l10n_br_contract/models/contract_contract.py
@@ -13,6 +13,10 @@ class ContractContract(models.Model):
     )
     country_id = fields.Many2one(related="company_id.country_id", store=True)
 
+    contract_recalculate_taxes_before_invoice = fields.Boolean(
+        default="company.contract_recalculate_taxes_before_invoice"
+    )
+
     @api.model
     def _fiscal_operation_domain(self):
         domain = [("state", "=", "approved")]
