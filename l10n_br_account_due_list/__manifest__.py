c2cfd9878f9c318b0ec2f452a3e43177682f3bac
Ravi do Valle Luz <ravi.luz@alumni.usp.br>
2024-04-08 16:28:45 -0300

    17.0 Migration: l10n_br_account_due_list

--- l10n_br_account_due_list/__manifest__.py
+++ l10n_br_account_due_list/__manifest__.py
@@ -1,4 +1,5 @@
 # Copyright (C) 2021 - TODAY Renato Lima - Akretion
+# Copyright (C) 2024 - Ravi do Valle Luz - XippTech
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
@@ -6,9 +7,9 @@
     "category": "Localisation",
     "development_status": "Production/Stable",
     "license": "AGPL-3",
-    "author": "Akretion, Odoo Community Association (OCA)",
+    "author": "Akretion, XippTech, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/l10n-brazil",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "depends": ["account_due_list"],
     "data": [
         "views/account_invoice_view.xml",
--- l10n_br_account_due_list/models/account_move.py
+++ l10n_br_account_due_list/models/account_move.py
@@ -1,10 +1,11 @@
 # Copyright (C) 2021 - TODAY Renato Lima - Akretion
+# Copyright (C) 2024 Ravi do Valle Luz - XippTech
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import api, fields, models
 
 
-class AccountInvoice(models.Model):
+class AccountMove(models.Model):
     _inherit = "account.move"
 
     financial_move_line_ids = fields.Many2many(
@@ -22,20 +23,26 @@ class AccountInvoice(models.Model):
         store=True,
     )
 
-    @api.depends("line_ids", "state")
+    @api.depends("line_ids.account_type")
     def _compute_financial(self):
         for move in self:
             lines = move.line_ids.filtered(
-                lambda l: l.account_id.account_type
+                lambda l: l.account_type
                 in ("asset_receivable", "liability_payable")
             )
             move.financial_move_line_ids = lines.sorted()
 
-    @api.depends("line_ids.amount_residual")
+    @api.depends(
+        "line_ids.account_type",
+        "line_ids.matched_debit_ids.credit_amount_currency",
+        "line_ids.matched_debit_ids.debit_move_id",
+        "line_ids.matched_credit_ids.debit_amount_currency",
+        "line_ids.matched_credit_ids.credit_move_id",
+    )
     def _compute_payments(self):
         for move in self:
             (
                 invoice_partials,
-                exchange_diff_moves,
+                _,
             ) = move._get_reconciled_invoices_partials()
             move.payment_move_line_ids = [partial[2].id for partial in invoice_partials]
