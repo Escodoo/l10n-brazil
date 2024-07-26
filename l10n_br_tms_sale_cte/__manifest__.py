5cbe09f3919a7b785709e2dd123d8be98adce553
Marcel Savegnago <marcel.savegnago@gmail.com>
2024-07-26 16:23:35 -0300

    [ADD] l10n_br_tms_sale_cte: add new module

---
+++ l10n_br_tms_sale_cte/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "L10n Br Tms Sale Cte",
+    "summary": """
+        Brazilian Localization TMS Sale CTe""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-brazil",
+    "depends": [
+        "l10n_br_tms_sale",
+        "l10n_br_cte",
+    ],
+    "data": [
+        "views/l10n_br_fiscal_document.xml",
+    ],
+    "demo": [],
+}
