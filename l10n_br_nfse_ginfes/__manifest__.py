af5bda417c6161a0ee5a91b5f2bd0c9451babc9a
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2025-11-10 13:33:30 -0300

    [IMP] l10n_br_nfse_ginfes: remove version specifier dependency

--- l10n_br_nfse_ginfes/__manifest__.py
+++ l10n_br_nfse_ginfes/__manifest__.py
@@ -13,10 +13,10 @@
     "development_status": "Beta",
     "external_dependencies": {
         "python": [
-            "erpbrasil.edoc>=2.5.2",
-            "erpbrasil.assinatura>=1.7.0",
-            "erpbrasil.transmissao>=1.1.0",
-            "erpbrasil.base>=2.3.0",
+            "erpbrasil.edoc",
+            "erpbrasil.assinatura",
+            "erpbrasil.transmissao",
+            "erpbrasil.base",
             "nfselib.ginfes",
         ],
     },
